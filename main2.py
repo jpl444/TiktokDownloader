9a6823c09035848e104ee18b0eb2e4301bf2b4d5
jpl444 <jpl44@protonmail.com>
2024-04-24 21:22:03 -0500

    Update .env.example with API credentials

---
+++ main2.py
@@ -0,0 +1,14 @@
+import asyncio
+from douyin_tiktok_scraper.scraper import Scraper
+
+api = Scraper()
+
+
+async def hybrid_parsing(url: str) -> dict:
+    # Hybrid parsing(Douyin/TikTok URL)
+    result = await api.hybrid_parsing(url)
+    print(f"The hybrid parsing result:\n {result}")
+    return result
+
+
+asyncio.run(hybrid_parsing(url=input("Paste Douyin/TikTok/Bilibili share URL here: ")))
